598606279bd4e43a43032b90a7eecdb2fd47b248
eliteGoblinRB <elitegoblinrb@gmail.com>
2023-03-31 08:22:27 +1000

    Add nested pie chart function

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 
 ![image](https://user-images.githubusercontent.com/4377212/226231316-d0e3f8df-8ec7-43d2-a37a-579d730c0721.png)  
 
-Personal time and metric tracking system, inspired by [Alexander Alexandrovich Lyubishchev](https://zh.wikipedia.org/wiki/%E4%BA%9A%E5%8E%86%E5%B1%B1%E5%A4%A7%C2%B7%E4%BA%9A%E5%8E%86%E5%B1%B1%E5%BE%B7%E7%BD%97%E7%BB%B4%E5%A5%87%C2%B7%E6%9F%B3%E6%AF%94%E6%AD%87%E5%A4%AB)'s personal time statistics method, mentioned in his bioriphy [This strange life](https://sudonull.com/post/171201-Granin-This-strange-life) 
+Personal time and metric tracking system, inspired by [Alexander Alexandrovich Lyubishchev](https://zh.wikipedia.org/wiki/%E4%BA%9A%E5%8E%86%E5%B1%B1%E5%A4%A7%C2%B7%E4%BA%9A%E5%8E%86%E5%B1%B1%E5%BE%B7%E7%BD%97%E7%BB%B4%E5%A5%87%C2%B7%E6%9F%B3%E6%AF%94%E6%AD%87%E5%A4%AB)'s personal time statistics method, illustrated in his bioriphy [This strange life](https://sudonull.com/post/171201-Granin-This-strange-life) 
+
+> In any field, any profession, this method can help you achieve significant results!   
+> Even if your talent is quite mediocre, this method can still guarantee you the greatest achievements.
 
 ## Setup
 
--- time_report_notebook/nb_helper_time_stats.py
+++ time_report_notebook/nb_helper_time_stats.py
@@ -1,4 +1,7 @@
+from typing import Union
+
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def time_stat_to_pie_chart_data(time_stat: dict[str, int]) -> dict[str, int]:
@@ -19,6 +22,20 @@ def time_stat_to_pie_chart_data(time_stat: dict[str, int]) -> dict[str, int]:
     return res
 
 
+def create_array(data: dict[str, Union[dict[str, int], int]]) -> np.ndarray:
+    rows = []
+
+    for key in data:
+        if isinstance(data[key], dict):
+            row = list(data[key].values())
+            rows.append(row)
+        elif isinstance(data[key], int):
+            row = [data[key], 0]
+            rows.append(row)
+
+    return np.array(rows)
+
+
 def show_time_stat_as_piechart(report_time_stat: dict[str, int]) -> None:
     """
     show time stat as a pie chart
@@ -28,4 +45,77 @@ def show_time_stat_as_piechart(report_time_stat: dict[str, int]) -> None:
     _, axis = plt.subplots()
     axis.pie(piechart_data.values(), labels=piechart_data.keys(), autopct="%1.1f%%")
     axis.set_title("Pie Chart of Data")
+    plt.show()
+
+
+def show_nested_piechart(values_dict: dict[str, Union[dict[str, int], int]]) -> None:
+    """
+    Support up to 2 levels of piechart, i.e dict nested level is 1 or 2
+    values_dict is a dict of main category and its sub categories
+    {
+        "sleep": {
+            "night_sleep": 380,
+            "nap": 60,
+        },
+        work: 480,
+        exercise: {
+            "jogging": 60,
+            "yoga": 30,
+        },
+    }
+    """
+
+    _, axis = plt.subplots()
+
+    size = 0.3
+    matrix = create_array(values_dict)
+
+    row, col = matrix.shape
+    cmap = plt.get_cmap("tab20c")
+
+    # color index in color map, closer means color is similar
+    outer_color_values = (
+        np.arange(row) * 4
+    )  # 4 is arbeitrary number, to make outter piechart color different
+    inner_color_value: list[int] = []
+
+    outer_labels = list(values_dict.keys())
+    inner_labels = []
+
+    for key in values_dict:
+        if isinstance(values_dict[key], dict):
+            inner_labels += list(values_dict[key].keys())
+        elif isinstance(values_dict[key], int):
+            inner_labels.append(key)
+
+    # pickup inner colors near value of color of its parent pie slice
+    for i in range(row):
+        for j in range(col):
+            if matrix[i][j] != 0:
+                inner_color_value.append(outer_color_values[i] + j)
+
+    # Create outer pie chart with labels
+    axis.pie(
+        matrix.sum(axis=1),
+        radius=1,
+        colors=cmap(outer_color_values),
+        labels=outer_labels,
+        wedgeprops=dict(width=size, edgecolor="w"),
+        autopct="%.1f%%",
+        pctdistance=0.85,
+    )
+
+    # Create inner pie chart with labels
+    non_zero_values = matrix.flatten()[matrix.flatten() != 0]
+    axis.pie(
+        non_zero_values,
+        radius=1 - size,
+        colors=cmap(inner_color_value),
+        labels=inner_labels,
+        wedgeprops=dict(width=size, edgecolor="w"),
+        autopct="%.1f%%",
+        pctdistance=0.75,
+    )
+
+    axis.set(aspect="equal", title="Pie plot with `ax.pie`")
     plt.show()
