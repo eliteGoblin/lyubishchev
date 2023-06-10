1fd27b03af9b28fc60de92ea3ea2d459c281e6ce
eliteGoblinRB <elitegoblinrb@gmail.com>
2023-06-10 20:39:03 +1000

    add relative month: this month, previous offset month report

--- lyubishchev/cli/__init__.py
+++ lyubishchev/cli/__init__.py
@@ -1,9 +1,10 @@
-from .report_cli import dayrange, last, relative_week
+from .report_cli import dayrange, last, relative_month, relative_week
 from .time_intervals import fetch_time_intervals_events_since_wakeup
 
 __all__ = [
     "dayrange",
     "last",
     "relative_week",
+    "relative_month",
     "fetch_time_intervals_events_since_wakeup",
 ]
